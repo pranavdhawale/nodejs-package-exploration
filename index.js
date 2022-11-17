0de6d9f46abc191f5ea3bcae03ba89d5ead79ca8
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-11-17 11:48:35 +0530

    feat: successfully import data from excel in nodejs

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const express = require('express');
-const app = express();
-
 const upload = require('express-fileupload');
 const importExcel = require('convert-excel-to-json');
 
-app.get('/', (req, res) => {
-    console.log('testng');
-});
+const app = express();
+app.use(upload());
 
-app.get('/upload', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/', (req, res) => {
+    if(!req.files) {
+        res.send('No file uploaded');
+    }
+    else {
+        console.log(req.files);
+        let file = req.files.file;
+        let filename = file.name;
+        file.mv('./excel/' + filename, (err) => {
+                if(err) {
+                    res.send(err);
+                }
+                else {
+                    let result = importExcel({
+                        sourceFile: './excel/' + filename,
+                        header: {
+                            rows: 1
+                        },
+                        columnToKey: {
+                            A: 'name',
+                            B: 'password'
+                        },
+                        sheets: ['Sheet1']
+                    });
+                    res.send(result);
+                    // console.log(result);
+                    // res.send('File ' + filename + ' uploaded');
+                    del(['./excel/' + filename])
+                        .then(paths => {
+                            console.log('Deleted files and folders:\n', paths.join('\n'));
+                        });
+                }
+            });
+    }
+});
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
