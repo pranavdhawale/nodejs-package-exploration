3a987d3654f75c45fe514bb7aff6ba5822a0bbdc
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-11-20 15:32:11 +0530

    feat: successfully send mail to self account

--- emailjs/src/App.jsx
+++ emailjs/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import EmailJS from "./emailjs";
 
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+    <EmailJS />
     </>
   )
 }
---
+++ emailjs/src/emailjs.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import emailjs from "emailjs-com";
+
+export default function EmailJS() {
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_6m8e34d', 'template_zq8ufpg', e.target, 'R1lPD0MhKamSF5unB')
+            .then((result) => {
+                console.log(result.text);
+                alert("Message sent successfully!");
+            }, (error) => {
+                console.log(error.text);
+                alert("Message failed to send.");
+            });
+        e.target.reset()
+    }
+
+    return (
+        <>
+        <form onSubmit={sendEmail}>
+            <div className="flex flex-col">
+                <div className="">
+                    <label>Name</label>
+                    <input className="border" type="text" name="user_name" />
+                </div>
+                <div className="">
+                    <label>Email</label>
+                    <input className="border" type="email" name="user_email" />
+                </div>
+                <div>
+                    <label>Message</label>
+                    <textarea className="border" name="message" />
+                </div>
+                <div>
+                    <input className="border" type="submit" value="Send" />
+                </div>
+            </div>
+        </form>
+        </>
+    )
+}
