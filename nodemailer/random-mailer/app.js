19b2a53e0dbbbd3668638a4d34cc587a536d40a8
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-11-20 16:47:14 +0530

    feat: successfully sends email to desired email address from OUTLOOK

--- nodemailer/random-mailer/app.js
+++ nodemailer/random-mailer/app.js
@@ -1,16 +1,56 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const router = express.Router();
+
 const port = 3000;
 
 // import nodemailer controller
-const controller = require('./nodemailer');
+// const controller = require('./nodemailer');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html');
 });
 
-router.get('/send', controller.send);
+app.post('/', (req, res) => {
+    console.log(req.body);
+    const { faculty_name, org_email, subject, message, link } = req.body;
+
+    res.send("Email Sent Successfully!");
+
+    const nodemailer = require('nodemailer');
+
+    const transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+            user: 'pranavdhawale19@outlook.com',
+            pass: 'OutlooK_pranav19'
+        }
+    });
+
+    const mailOptions = {
+        from: 'pranavdhawale19@outlook.com',
+        to: org_email,
+        subject: subject,
+        text: 'Hello!\n' + faculty_name + ' here,' + message + '\n\n' + link
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+});
+
+// POST route to send mail
+// router.post('/send', controller.send);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
