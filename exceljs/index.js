63e7189c931848d2242d5118b7da32686084d5c8
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-11-21 09:43:15 +0530

    feat: successfully write data from Local SpreadSheet to Google Sheets

--- exceljs/index.js
+++ exceljs/index.js
@@ -36,55 +36,15 @@ async function gsrun(cl) {
 
     // discard sheet 1 empty item
     data1.shift();
-    console.log(data1);
-
-    // get data from Sheet2
-    let sheet2 = excelFile.getWorksheet('Sheet2');
-    let data2 = sheet2.getSheetValues();
-
-    // discard 1 empty item from each row
-    data2 = data2.map(function(r) {
-        return [r[1],r[2],r[3]]
-    });
-
-    // discard sheet 1 empty item
-    data2.shift();
-    console.log(data2);
+    // console.log(data1);
     
-    // // get metadata about spreadsheet
-    // const opt = {
-    //     spreadsheetId: '1j9j26-B9c1NIZ7YcNrhVISXJHxfrFDFbTnveu6-zPfo',
-    //     range: 'A2:B5'
-    // };
+    //update function
+    const update = {
+        spreadsheetId: '1j9j26-B9c1NIZ7YcNrhVISXJHxfrFDFbTnveu6-zPfo',
+        range: 'LocalExcelData!A1',
+        valueInputOption: 'USER_ENTERED',
+        resource: { values: data1 }
+    };
 
-    // // read data from spreadsheet
-    // let data = await gsapi.spreadsheets.values.get(opt);
-    // let dataArray = data.data.values;
-    // // console.log(dataArray);
-
-    // // convert undefined data to empty string
-    // dataArray = dataArray.map(function(r) {
-    //     while(r.length < 2) {
-    //         r.push('');
-    //     }
-    //     return r;
-    // });
-    // console.log(dataArray);
-
-    // // update data
-    // let newDataArray = dataArray.map(function(r) {
-    //     r.push(r[0] + '-' + r[1]);
-    //     return r;
-    // })
-    // // console.log(newDataArray);
-    
-    // //update function
-    // const update = {
-    //     spreadsheetId: '1j9j26-B9c1NIZ7YcNrhVISXJHxfrFDFbTnveu6-zPfo',
-    //     range: 'C2',
-    //     valueInputOption: 'USER_ENTERED',
-    //     resource: { values: newDataArray }
-    // };
-
-    // let res = await gsapi.spreadsheets.values.update(update);
+    let res = await gsapi.spreadsheets.values.update(update);
 }
