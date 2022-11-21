d4ee3543735734bd498cda8e4eed6e788109571a
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-11-21 08:58:29 +0530

    fix: convert undefined data/blank data to empty string

--- googleapis/index.js
+++ googleapis/index.js
@@ -20,20 +20,35 @@ client.authorize(function(err, tokens) {
 
 async function gsrun(cl) {
     const gsapi = google.sheets({ version: 'v4', auth: cl });
+    
+    // get metadata about spreadsheet
     const opt = {
         spreadsheetId: '1j9j26-B9c1NIZ7YcNrhVISXJHxfrFDFbTnveu6-zPfo',
         range: 'A2:B5'
     };
 
+    // read data from spreadsheet
     let data = await gsapi.spreadsheets.values.get(opt);
     let dataArray = data.data.values;
     // console.log(dataArray);
+
+    // convert undefined data to empty string
+    dataArray = dataArray.map(function(r) {
+        while(r.length < 2) {
+            r.push('');
+        }
+        return r;
+    });
+    console.log(dataArray);
+
+    // update data
     let newDataArray = dataArray.map(function(r) {
         r.push(r[0] + '-' + r[1]);
         return r;
     })
     // console.log(newDataArray);
     
+    //update function
     const update = {
         spreadsheetId: '1j9j26-B9c1NIZ7YcNrhVISXJHxfrFDFbTnveu6-zPfo',
         range: 'C2',
@@ -42,5 +57,4 @@ async function gsrun(cl) {
     };
 
     let res = await gsapi.spreadsheets.values.update(update);
-    console.log(res);
 }
