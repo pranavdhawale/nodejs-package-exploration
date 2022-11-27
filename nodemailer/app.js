bf478ad5b0cc5deeefc5f61419772dfca176d78e
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-11-27 12:28:38 +0530

    successfully send mail from google account

--- nodemailer/app.js
+++ nodemailer/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// import .env variables
+require('dotenv').config();
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const router = express.Router();
-
-const port = 3000;
+const port = 4000;
 
 // import nodemailer controller
 // const controller = require('./nodemailer');
@@ -18,26 +19,23 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body);
-    const { faculty_name, org_email, subject, message, link } = req.body;
-
-    res.send("Email Sent Successfully!");
-
     const nodemailer = require('nodemailer');
 
     const transporter = nodemailer.createTransport({
-        service: 'hotmail',
+        service: 'gmail',
         auth: {
-            user: 'pranavdhawale19@outlook.com',
-            pass: 'OutlooK_pranav19'
-        }
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        },
+        port: 465,
+        host: 'smtp.gmail.com'
     });
 
     const mailOptions = {
-        from: 'pranavdhawale19@outlook.com',
-        to: org_email,
-        subject: subject,
-        text: 'Hello!\n' + faculty_name + ' here,' + message + '\n\n' + link
+        from: process.env.EMAIL,
+        to: "umangsomani7@gmail.com",
+        subject: "Sending Email using Node.js",
+        text: 'Hello!\n' + 'Pranav here'
     };
 
     transporter.sendMail(mailOptions, function(error, info){
@@ -45,12 +43,11 @@ app.post('/', (req, res) => {
             console.log(error);
         } else {
             console.log('Email sent: ' + info.response);
+            res.send('Email sent: ' + info.response);
         }
     });
 });
 
-// POST route to send mail
-// router.post('/send', controller.send);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
