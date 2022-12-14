d183a97f3870fba55d3a7bd66c7c2e6c9584c0d7
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-14 07:05:11 +0530

    route pages and use context

--- passport-jwt/frontend/src/App.jsx
+++ passport-jwt/frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+
+import { Token } from "./globalContext";
 
 export default function App() {
+  const [token, setToken] = useState('')
   return(
     <>
-      
+    <Token.Provider value={[token, setToken]}>
+        <Routes>
+          <Route path="/" element={<Login />}/>
+          <Route path="/dashboard" element={<Dashboard />}/>
+        </Routes>
+      </Token.Provider>
     </>
   )
 }
